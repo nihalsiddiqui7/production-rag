4df2d78c042cc916ff01e020c8db54b5f661c8e7
Nihal Naser Siddiqui <nihal070502@gmail.com>
2026-06-13 16:39:34 +0530

    tested notebook

--- app/ingestion/loader.py
+++ app/ingestion/loader.py
@@ -1,3 +1,4 @@
+from json import load
 from pathlib import Path
 from unittest import loader
 from langchain_community.document_loaders import PyPDFLoader
@@ -7,21 +8,26 @@ from langchain_community.document_loaders import TextLoader
 
 
 
-class PDFLoader:
-    def __init__(self,pdf_path: str):
-        self.pdf_path = pdf_path
+# class PDFLoader:
+#     def __init__(self,pdf_path: str):
+#         self.pdf_path = pdf_path
 
-    def load(self):
-        if not Path(self.pdf_path).exists():
-            raise FileNotFoundError(f"File {self.pdf_path} does not exist.")
+#     def load(self):
+#         if not Path(self.pdf_path).exists():
+#             raise FileNotFoundError(f"File {self.pdf_path} does not exist.")
         
 
-        # loader = PyPDFLoader(self.pdf_path)
+#         loader = PyPDFLoader(self.pdf_path)
 
-        # documents = loader.load()
+#         documents = loader.load()
 
-        loader = TextLoader(self.pdf_path)
-        documents = loader.load()
-        return documents
+#         return documents
+
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+
+def doc_loader(pdf_path: str):
+    loader = PyPDFLoader(pdf_path) 
+    documents = loader.load()
+    return documents
     
     
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='production-rag',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='A production-ready Retrieval-Augmented Generation (RAG) system.',
+    author='Nihal Siddiqui'
+)
